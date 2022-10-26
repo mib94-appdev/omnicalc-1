2f901cc80d02e95bfc1b85f97fbe2fab5da643f2
mib94 <mibnobachir@chicagobooth.edu>
2022-10-26 21:57:29 +0000

    completed omnicalc 1 hw

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,47 @@ class ApplicationController < ActionController::Base
 
     render({ :template => "calculation_templates/square_results.html.erb" })
   end
+
+  def blank_random
+
+    render({ :template => "calculation_templates/rand_form.html.erb" })
+  end
+
+  def calculate_random
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+
+    render({ :template => "calculation_templates/rand_results.html.erb" })
+  end
+
+  def blank_square_root
+
+    render({ :template => "calculation_templates/sqrt_form.html.erb"})
+  end
+
+  def calculate_square_root
+    @numb = params.fetch("square_root").to_f
+    @sqrt_of_numb = @numb ** 0.5
+
+    render({ :template => "calculation_templates/sqrt_results.html.erb"})
+  end
+
+  def blank_payment
+
+    render({ :template => "calculation_templates/payment_form.html.erb"})
+  end
+
+  def calculate_payment
+    @apr = params.fetch("user_apr").to_f / 100
+    @years = params.fetch("user_years").to_f
+    @n = @years * 12
+    @principal = params.fetch("user_principal").to_f
+    @r = @apr / 12
+    @numerator = @r * @principal
+    @denominator = 1 - (1/(1 + @r))**(@n)
+    @payment = @numerator / @denominator
+
+    render({ :template => "calculation_templates/payment_results.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
   get("/square/new", { :controller => "application", :action => "blank_square_form" })
 
   get("/square/results", { :controller => "application", :action => "calculate_square" })
+
+  get("/random/results", { :controller => "application", :action => "calculate_random" })
+
+  get("/random/new", { :controller => "application", :action => "blank_random" })
+
+  get("/square_root/new", { :controller => "application", :action => "blank_square_root" })
+
+  get("/square_root/results", { :controller => "application", :action => "calculate_square_root" })
+
+  get("/payment/new", { :controller => "application", :action => "blank_payment" })
+
+  get("/payment/results", { :controller => "application", :action => "calculate_payment" })
+
 end
